870855dd211c1f3a16c189f51cd0b188c72354d4
Vainius <Vainius@PC737>
2020-01-02 13:46:47 +0200

    deleted tutorial

--- MVCTutorialDB/dbo/Tables/Employee.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [EmployeeId] INT NOT NULL, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [EmailAddress] NVARCHAR(50) NOT NULL
-)
